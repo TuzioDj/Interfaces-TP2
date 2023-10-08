c2f78045feea9786bf18885b4101637bc77954ee
Esteban Orellano <esteban.orellano.912@gmail.com>
2023-10-08 16:40:31 -0300

    captcha hecho

--- js/login.js
+++ js/login.js
@@ -9,33 +9,4 @@ botonAlternativa.forEach(boton => {
     })
 })
 
-document.addEventListener("DOMContentLoaded", function () {
-    const loadingContainer = document.querySelector(".loading-container");
-    const loadingProgress = document.getElementById("loading-progress");
 
-
-    const cargaTotal = 100;
-    const intervalo = 50; 
-    let progreso = 0;
-
-    const actualizarPorcentaje = () => {
-        progreso++;
-        loadingProgress.textContent = progreso ;
-    };
-
-    const cargarContenidoPrincipal = () => {
-        setTimeout(() => {
-            loadingContainer.style.display = "none"; 
-        }, 5000); 
-
-        const intervaloCarga = setInterval(() => {
-            if (progreso < cargaTotal) {
-                actualizarPorcentaje();
-            } else {
-                clearInterval(intervaloCarga);
-            }
-        }, intervalo);
-    };
-
-    cargarContenidoPrincipal(); 
-});
